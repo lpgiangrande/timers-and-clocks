14489bc0661a0d6cf9b28d21884bb7669460e9b6
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-04-11 01:06:24 +0200

    addLeadingZero func

--- digital-clock/refactored.js
+++ digital-clock/refactored.js
@@ -5,16 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const now = new Date();
 
     const hours = now.getHours();
-    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const minutes = now.getMinutes();
 
     /**
-     * Convert to 12-hour format (01 PM pour 13H)
+     * Convert a single digit into a string and add a zero before
+     * @param {number} n a digit between 0 and 9
+     * @returns {string} 01 instead of 1
+     */
+    function addLeadingZero(n){
+        return n.toString().padStart(2, '0');
+    }
+
+    /**
+     * Convert to 12-hour format (01 PM pour 13h)
      * --> Returns 03h PM instead of 15h (15%12 = 3) or 12 instead of 0 for 12%12. 
      * --> the modulo operation on a number n that is less than the divisor simply returns n
      */
     const hours12 = hours % 12 || 12; 
-    hoursDiv.innerHTML = hours12.toString().padStart(2, '0');
-    minutesDiv.innerHTML = minutes;
+    hoursDiv.innerHTML = addLeadingZero(hours12);
+    minutesDiv.innerHTML = addLeadingZero(minutes);
     ampmDiv.innerHTML = hours >= 12 ? 'PM' : 'AM';
     
 

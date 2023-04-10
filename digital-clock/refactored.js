a75b140db6ec8d93e24571d5eec0a3a446b8f236
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-04-10 17:29:35 +0200

    refactored js script

--- digital-clock/refactored.js
+++ digital-clock/refactored.js
@@ -1,31 +1,25 @@
-// make sure code runs only after the HTML content has been loaded :
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Get the #hours and # minutes div element
     const [hoursDiv, separatorDiv, minutesDiv] = document.querySelectorAll("#clock > div");
 
-    // Get the current date and time
     const now = new Date();
-  
-    // Get the hours value
+
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
   
-    // Set the #hours div element's innerHTML to the hours value
     hoursDiv.innerHTML = hours;
     minutesDiv.innerHTML = minutes;
 
 
 
-    // Make separatorDiv blink every second
+    // Make separatorDiv " : " blink every second
 
     // Set an initial value for the visibility of the separator
     let separatorVisible = true;
     
-    // Create a timer that runs every second
-    setInterval(() => {
-        // Toggle the visibility of the separator
-        separatorVisible = !separatorVisible;
+    setInterval(() => {   // Create a timer that runs every second
+        
+        separatorVisible = !separatorVisible; // Toggle the visibility of the separator
   
         // Set the visibility of the separator
         separatorDiv.style.visibility = separatorVisible ? "visible" : "hidden";

964cb822551f37d481d10bafc65386ed2bbc7d8e
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-04-11 04:47:50 +0200

    add audio, reset

--- pomodoro-timer/script.js
+++ pomodoro-timer/script.js
@@ -2,19 +2,10 @@
  * POMODORO TIMER
  */
 
-
-/**
- * Change tab color when time is up.
- */
-function changeTabColor() {
-    document.title = "Time's up!";
-    let tabColor = "#FF0000"; // set the color of the tab
-    document.querySelector("meta[name='theme-color']").setAttribute("content", tabColor);
-}
-
 const timerDisplay = document.querySelector('.time-display');
 const startButton = document.querySelector('.start-button');
 const resetButton = document.querySelector('.reset-button');
+const alarmSound = new Audio('./oversimplified-alarm-clock.mp3');
 
 let timeLeft = 25 * 60; // 25 minutes in seconds
 let timerId; // store the interval ID returned by setInterval() when the timer is started
@@ -27,9 +18,27 @@ function startTimer() {
       if (timeLeft <= 0) {
         clearInterval(timerId);
         timerId = null;
-        alert('Time is up!');
+        //alert('Time is up!');
+        alarmSound.play(); 
       }
     }, 1000);
   }
 }
 
+function resetTimer() {
+  clearInterval(timerId);
+  timerId = null;
+  timeLeft = 25 * 60;
+  updateTimerDisplay();
+  alarmSound.pause(); // stop the alarm sound when resetting the timer
+  alarmSound.currentTime = 0; // reset the alarm sound to the beginning
+}
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(timeLeft / 60).toString().padStart(2, '0');
+  const seconds = (timeLeft % 60).toString().padStart(2, '0');
+  timerDisplay.textContent = `${minutes}:${seconds}`;
+}
+
+startButton.addEventListener('click', startTimer);
+resetButton.addEventListener('click', resetTimer);
